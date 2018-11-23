c55b875e76102bd6e2040f96bc806b854129020b
Anders Rønningen <anders@ronningen.priv.no>
2018-11-23 08:56:34 +0100

    Add top-level setup.sh

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+# Check os
+case "$(uname -a)" in
+    *Ubuntu*)   OS="ubuntu";;
+    Darwin*)    OS="osx";;
+    *)          echo "unknown os"; exit 1;;
+esac
+
+MODULES=()
+
+# Install prerequisite packages
+if [ "$OS" == "ubuntu" ]; then
+    for MODULE in "${MODULES[@]}"; do
+        cat modules/$MODULE/packages.apt 2>/dev/null
+    done | sort -u | sudo xargs apt install -y
+else
+    brew update
+    for MODULE in "${MODULES[@]}"; do
+        cat modules/$MODULE/packages.brew 2>/dev/null
+    done | sort -u | brew install
+fi
+
+# Run module setup scripts
+for MODULE in "${MODULES[@]}"; do
+    modules/$MODULE/setup.sh
+done
