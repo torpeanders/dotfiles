db98a0937964325a7335d14e0aa6d01346ede06e
Anders Rønningen <anders@ronningen.priv.no>
2019-02-23 22:50:52 +0100

    add i3 again

--- setup.sh
+++ setup.sh
@@ -8,9 +8,10 @@ case "$(uname -a)" in
 esac
 
 MODULES=($OS os-common bin emacs mpv tmux vim zsh)
-#if dpkg -l ubuntu-desktop > /dev/null 2>&1; then
-#    MODULES+=(i3 x11)
-#fi
+if dpkg -l ubuntu-desktop > /dev/null 2>&1; then
+    #MODULES+=(i3 x11)
+    MODULES+=(i3)
+fi
 
 # Install prerequisite packages
 if [ "$OS" == "ubuntu" ]; then
