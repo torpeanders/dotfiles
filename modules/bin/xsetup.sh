9116657b94f8c5e5d83e775d67fc4142ed590e65
Anders Rønningen <anders.ronningen@neatframe.com>
2019-02-28 11:55:22 +0100

    Revert "Throw out xsettingsd."
    This reverts commit ac1af71887356e833f3d5f3c94c9f7b60f8aad87.

--- modules/bin/xsetup.sh
+++ modules/bin/xsetup.sh
@@ -9,31 +9,55 @@ FULL_DPI=($(cat $XRANDR_TEMP | perl -ne 'if (/connected primary (\d+)x(\d+).* (\
 REAL_DPI=${FULL_DPI[0]}
 DPI=${FULL_DPI[1]}
 
-file=$HOME/.Xresources
 
-if ! grep -q "Xft.antialias" $file; then
-    echo "Xft.antialias:  true" >> $file
+file=$HOME/.xsettingsd
+
+if ! grep -q Xft/DPI $file; then
+    echo Xft/DPI $[96*1024] >> $file
 fi
 
-if ! grep -q "Xft.dpi" $file; then
-    echo "Xft.dpi: 96" >> $file
+if ! grep -q Gdk/UnscaledDPI $file; then
+    echo Gdk/UnscaledDPI $[96*1024] >> $file
 fi
 
-if ! grep -q "Xft.hinting" $file; then
-    echo "Xft.hinting: true" >> $file
+ed -s $file <<EOF
+g,Gdk/UnscaledDPI,d
+a
+Gdk/UnscaledDPI $DPI
+.
+g,Xft/DPI,d
+a
+Xft/DPI $DPI
+.
+w
+q
+EOF
+
+file2=$HOME/.Xresources
+
+if ! grep -q "Xft.antialias" $file2; then
+    echo "Xft.antialias:  true" >> $file2
 fi
 
-if ! grep -q "Xft.hintstyle" $file; then
-    echo "Xft.hintstyle: true" >> $file
+if ! grep -q "Xft.dpi" $file2; then
+    echo "Xft.dpi: 96" >> $file2
 fi
 
-#if ! grep -q "Xft.rgba" $file; then
-#    echo "Xft.rgba: rgb" >> $file
+if ! grep -q "Xft.hinting" $file2; then
+    echo "Xft.hinting: true" >> $file2
+fi
+
+if ! grep -q "Xft.hintstyle" $file2; then
+    echo "Xft.hintstyle: true" >> $file2
+fi
+
+#if ! grep -q "Xft.rgba" $file2; then
+#    echo "Xft.rgba: rgb" >> $file2
 #fi
 
 xrandr --dpi $REAL_DPI
 
-ed -s $file <<EOF
+ed -s $file2 <<EOF
 g,Xft.dpi,d
 a
 Xft.dpi: $REAL_DPI
@@ -42,4 +66,5 @@ w
 q
 EOF
 
-xrdb -merge $file
+xrdb -merge $file2
+xsettingsd &
